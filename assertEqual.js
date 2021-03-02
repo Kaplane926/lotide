cfbf96434a28e5e6c9ed594b1eb4f59050f088a4
Eli Kaplan <elikaplan@me.com>
2021-03-02 01:32:07 +0000

    just some eslint syntax fixes

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,11 @@
-const assertEqual = function(actual, expected){
-  if(actual !== expected){
-    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  } else {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
   }
-    else{
-      return `✅✅✅ Assertion Passed: ${actual} === ${expected}`
-    }
 
-}
+};
 
 // TEST CODE
 console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
