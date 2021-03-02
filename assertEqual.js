23e8450a2fff3e751e95d7e29c903fafcb8decc4
Eli Kaplan <elikaplan@me.com>
2021-03-02 00:40:29 +0000

    wrote our own console.assert function, now with emojis!

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected){
+  if(actual !== expected){
+    return "🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected
+  }
+    else{
+      return "✅✅✅ Assertion Passed: " + actual + " === " + expected
+    }
+
+}
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual(1, 2));
+console.log(assertEqual("Hi", "Hi"));
