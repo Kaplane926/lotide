8fb82976a9a49411a170ddd1ccdb8cb544e190d5
Eli Kaplan <elikaplan@me.com>
2021-03-04 06:01:04 +0000

    added a function that will use a nice visual to tell me if objects are equal

---
+++ assertObjectsEqual.js
@@ -0,0 +1,63 @@
+const eqArrays = function(array1, array2){
+  if(array1.length !== array2.length){
+
+    return false
+
+  }
+  for(let i = 0; i < array1.length; i++){
+    if(array1[i] !== array2[i]){
+      return false
+    }
+  }
+  return true
+}
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+
+const eqObjects = function(object1, object2) {
+  if(Object.keys(object1).length !== Object.keys(object2).length){
+    return false
+  }
+  for (key in object1){
+    
+    if(Array.isArray(object1[key]) && Array.isArray(object2[key])){
+      if((eqArrays(object1[key], object2[key])) === false){
+        return false
+      }
+      
+    }else if
+    (object1[key] !== object2[key]){
+      return false
+    }
+  }
+  return true
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected) === false) {
+    return `🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`;
+  } else {
+    return `✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`;
+  }
+};
+
+const dominion = {durations: "orange",
+vicoryPoints: [3,5,6,10],
+players: [2,3,4,5,6],
+bestplayer: "kaplane"
+}
+
+const hominion = {durations: "orange",
+vicoryPoints: [3,5,6,10],
+players: [2,3,4,5,6],
+bestplayer: "kaplane"
+}
+
+const idgaf = {
+  someKey: "some value"
+}
+
+console.log(assertObjectsEqual(hominion, dominion))
+console.log(assertObjectsEqual(idgaf, dominion))
--- eqArrays.js
+++ eqArrays.js
@@ -27,3 +27,4 @@ console.log(assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false));
 console.log(assertEqual(eqArrays([5, 2, 3], [1, 2, 3]), false));
 console.log(assertEqual(eqArrays(["boolean", "joolean", "mcfoolean"], ["hi", "bye", "why"]), false));
 console.log(assertEqual(eqArrays(["boolean", "joolean", "mcfoolean"], ["boolean", "joolean", "mcfoolean"]), true));
+console.log(eqArrays(["2", 3], ["2", 3]))
