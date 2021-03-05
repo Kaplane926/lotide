f145466f6665af5a1ba27b1999b0a67826a75433
Eli Kaplan <elikaplan@me.com>
2021-03-05 02:27:00 +0000

    made a function that creates a new array until a given value

---
+++ takeUntil.js
@@ -0,0 +1,49 @@
+const eqArrays = function(array1, array2){
+  if(array1.length !== array2.length){
+
+    return false
+
+  }
+  for(let i = 0; i < array1.length; i++){
+    if(array1[i] !== array2[i]){
+      return false
+    }
+  }
+  return true
+}
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === false) {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  } else {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  }
+
+};
+
+
+const takeUntil = function(array, callback) {
+  newArray = []
+  for(element of array){
+    if(callback(element) === true){
+      break
+    }
+    newArray.push(element)
+  }
+  return newArray
+}
+
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+console.log(assertArraysEqual(results1, [1, 2, 5, 7, 2]))
+console.log(assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]))
